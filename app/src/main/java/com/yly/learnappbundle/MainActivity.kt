51fda5715fc9d8f6f16b730a75d3ad79cb0312e6
yuliyang <645644717@qq.com>
2021-11-29 14:57:58 +0800

    添加测试

--- app/src/main/java/com/yly/learnappbundle/MainActivity.kt
+++ app/src/main/java/com/yly/learnappbundle/MainActivity.kt
@@ -1,17 +1,25 @@
 package com.yly.learnappbundle
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.play.core.splitinstall.SplitInstallRequest
 import com.google.android.play.core.splitinstall.SplitInstallStateUpdatedListener
 import com.google.android.play.core.splitinstall.model.SplitInstallSessionStatus
+import com.google.android.play.core.splitinstall.testing.FakeSplitInstallManager
+import com.google.android.play.core.splitinstall.testing.FakeSplitInstallManagerFactory
 import java.util.*
+
+const val STORAGE_MODULE = "storage"
 
 class MainActivity : AppCompatActivity() {
 
     private var sessionId = 0
+    private lateinit var splitInstallManager: FakeSplitInstallManager
+    private var storageModule: MyAABInterface? = null
+
 
     companion object {
         const val TAG = "MainActivity"
@@ -43,11 +51,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        splitInstallManager = FakeSplitInstallManagerFactory.create(application)
+        splitInstallManager.registerListener(listener)
         findViewById<Button>(R.id.test).setOnClickListener {
             //通过SPI加载storage模块化
-            val services = ServiceLoader.load(MyAABInterface::class.java, MyAABInterface::class.java.classLoader)
-            val utils = services.iterator().next()
-            utils.test()
+//            val services = ServiceLoader.load(MyAABInterface::class.java, MyAABInterface::class.java.classLoader)
+//            val utils = services.iterator().next()
+//            utils.test()
+
+            //动态加载
+            if (storageModule == null) {
+                if (isStorageInstalled()) {
+                    initializeStorageFeature()
+                } else {
+                    requestStorageInstall()
+                }
+            }
+            if (storageModule != null) {
+                storageModule!!.test()
+                Toast.makeText(getApplication(), "Counter saved to storage", Toast.LENGTH_SHORT).show()
+            }
         }
+    }
+
+    private fun initializeStorageFeature() {
+        val services = ServiceLoader.load(MyAABInterface::class.java, MyAABInterface::class.java.classLoader)
+        storageModule = services.iterator().next()
+    }
+
+    private fun requestStorageInstall() {
+        Toast.makeText(getApplication(), "Requesting storage module install", Toast.LENGTH_SHORT)
+            .show()
+        val request =
+            SplitInstallRequest
+                .newBuilder()
+                .addModule("storage")
+                .build()
+
+        splitInstallManager
+            .startInstall(request)
+            .addOnSuccessListener { id -> sessionId = id }
+            .addOnFailureListener { exception ->
+                Log.e(TAG, "Error installing module: ", exception)
+                Toast.makeText(
+                    getApplication(),
+                    "Error requesting module install",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+    }
+
+    private fun isStorageInstalled() =
+        splitInstallManager.installedModules.contains(STORAGE_MODULE)
+
+    override fun onDestroy() {
+        splitInstallManager.unregisterListener(listener)
+        super.onDestroy()
     }
 }
